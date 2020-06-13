e2bd2ad041fa3af4cedbc2a37e97fa61be85ceb7
sassenagh <anna.alcaide.hijano@gmail.com>
2020-06-13 02:33:03 +0200

    Configure authentication login

---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\User;
+
+class UserRepository extends BaseRepository
+{
+    protected static function entityClass(): string
+    {
+        return User::class;
+    }
+
+    public function findOneByEmail(string $email): ?User
+    {
+        /** @var User $user */
+        $user = $this->objectRepository->findOneBy(['email' => $email]);
+
+        return $user;
+    }
+
+    public function save(User $user): void
+    {
+        $this->saveEntity($user);
+    }
+}
